412fdecf3c441ee92d26684c45d47f60aea34a60
Hideyuki Teramura <hide4d51@gmail.com>
2020-10-25 00:07:19 +0900

    Update the signing script

--- sign-repo.sh
+++ sign-repo.sh
@@ -2,8 +2,10 @@
 
 # x86_64
 pushd ikubakus-reserve/os/x86_64
+rm *.sig
+rm *.old
 mv ikubakus-reserve.db ikubakus-reserve.db.tar.gz
-for pkg in "$@"
+for pkg in $(ls *.zst)
 do
     gpg --detach-sign --use-agent --no-armor -u 00840ACBA2AA70E7 "$pkg"
     repo-add -v -s -k 00840ACBA2AA70E7 ikubakus-reserve.db.tar.gz "$pkg"
