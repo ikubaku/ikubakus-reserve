6d5b368b38c1586d7ed656d1fa32de09ef42866d
Hideyuki Teramura <hide4d51@gmail.com>
2020-10-24 19:09:39 +0900

    Fix path

--- sign-repo.sh
+++ sign-repo.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-repo-add -v -s -k 00840ACBA2AA70E7 ikubakus-reserve/ikubakus-reserve.db $@
+# x86_64
+repo-add -v -s -k 00840ACBA2AA70E7 ikubakus-reserve/os/x86_64/ikubakus-reserve.db $@
