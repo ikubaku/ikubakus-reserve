b651590b78fea21c34c95e56d5ed3084b8d967a1
Hideyuki Teramura <hide4d51@gmail.com>
2020-10-24 20:09:55 +0900

    Fix package signing

--- sign-repo.sh
+++ sign-repo.sh
@@ -6,6 +6,6 @@ mv ikubakus-reserve.db ikubakus-reserve.db.tar.gz
 for pkg in "$@"
 do
     gpg --detach-sign --use-agent --no-armor -u 00840ACBA2AA70E7 "$pkg"
+    repo-add -v -s -k 00840ACBA2AA70E7 ikubakus-reserve.db.tar.gz "$pkg"
 done
-repo-add -s -k 00840ACBA2AA70E7 ikubakus-reserve.db.tar.gz
 popd
