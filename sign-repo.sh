4827373668108279f3b4a90cb550952394c78fba
ikubaku <hide4d51@gmail.com>
2020-10-24 19:47:53 +0900

    Fix the signing script

--- sign-repo.sh
+++ sign-repo.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # x86_64
-repo-add -v -s -k 00840ACBA2AA70E7 ikubakus-reserve/os/x86_64/ikubakus-reserve.db $@
+pushd ikubakus-reserve/os/x86_64
+mv ikubakus-reserve.db ikubakus-reserve.db.tar.gz
+for pkg in "$@"
+do
+    gpg --detach-sign --use-agent --no-armor -u 00840ACBA2AA70E7 "$pkg"
+done
+repo-add -s -k 00840ACBA2AA70E7 ikubakus-reserve.db.tar.gz
+popd
